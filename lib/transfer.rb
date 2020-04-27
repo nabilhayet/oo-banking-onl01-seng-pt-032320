590871cf69ea9e77c82c0370e9cd72f9120b05be
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-27 01:31:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,8 +21,7 @@ class Transfer
           sender.balance -= self.amount
           receiver.balance += self.amount
           self.status = "complete"
-       
-      else 
+       else 
         self.status = "rejected"     
        "Transaction rejected. Please check your account balance."
       end 
