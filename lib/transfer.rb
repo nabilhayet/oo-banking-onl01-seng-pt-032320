ce0dfbacd46281c75be9807c166e3156d7648549
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-27 01:34:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer
     if self.status == "complete"
       sender.balance += self.amount
       receiver.balance -= self.amount
-      self.status = "pending"
+      self.status = "reversed"
   end 
 
 end 
